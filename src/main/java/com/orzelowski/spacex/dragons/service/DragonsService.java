4b5c8aea2d781e8a93678875d7a61961884927e6
orzelke <marcin.hubert.orzelowski@gmail.com>
2024-10-20 19:43:29 +0200

    Add junit, service methods declarations and test class.

--- src/main/java/com/orzelowski/spacex/dragons/service/DragonsService.java
+++ src/main/java/com/orzelowski/spacex/dragons/service/DragonsService.java
@@ -1,8 +1,34 @@
 package com.orzelowski.spacex.dragons.service;
 
+import com.orzelowski.spacex.dragons.enums.MissionStatus;
+import com.orzelowski.spacex.dragons.repository.MissionRepository;
+import com.orzelowski.spacex.dragons.repository.RocketRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class DragonsService {
 
+    private final MissionRepository missionRepository;
+    private final RocketRepository rocketRepository;
+
+    public DragonsService(MissionRepository missionRepository, RocketRepository rocketRepository) {
+        this.missionRepository = missionRepository;
+        this.rocketRepository = rocketRepository;
+    }
+
+    public void createRocket(String rocketName) {
+
+    }
+
+    public void assingRocketToMission(UUID rocketID, UUID missionID) {
+
+    }
+
+    public void createMission(String missionName) {}
+
+    public void changeMissionStatus(UUID missionID, MissionStatus missionStatus) {}
+
+    public String getMissionsSummary() {return "";}
 }
